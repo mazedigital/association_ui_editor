78dc8aa98c01b156564265088ebeba8debdacbc1
Nils Hörrmann <post@nilshoerrmann.de>
2014-06-10 10:26:36 +0200

    Provide Editor

--- extension.driver.php
+++ extension.driver.php
@@ -2,7 +2,32 @@
 
 Class extension_association_ui_editor extends Extension
 {
+    private static $provides = array();
 
+    public static function registerProviders()
+    {
+        self::$provides = array(
+            'association-editor' => array(
+                'Editor' => array('editor')
+            )
+        );
 
+        return true;
+    }
+
+    public static function providerOf($type = null)
+    {
+        self::registerProviders();
+
+        if (is_null($type)) {
+            return self::$provides;
+        }
+
+        if (!isset(self::$provides[$type])) {
+            return array();
+        }
+
+        return self::$provides[$type];
+    }
 
 }
