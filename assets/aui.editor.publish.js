e654d03393839798cd07bc09470d7db6aef1dd1f
Nils Hörrmann <post@nilshoerrmann.de>
2014-07-14 10:57:25 +0200

    Page introduction
    Related to #1, fixes #3

--- assets/aui.editor.publish.js
+++ assets/aui.editor.publish.js
@@ -1,31 +1,56 @@
 (function($, Symphony) {
 	'use strict';
 
+	Symphony.Language.add({
+		'Edit': false
+	});
+
 	Symphony.Extensions.AssociationUIEditor = function() {
-		var fields, trigger;
+		var fields,
+			templateTrigger, templatePage;
 
 		var init = function() {
-			// console.log('editor');
-			trigger = createTrigger();
 			fields = Symphony.Elements.contents.find('.field[data-editor^="aui-editor"]');
-			setTimeout(function() {
-				fields.each(attachEditor);
-			},0);
+			templateTrigger = createTriggerTemplate();
+			templatePage = createPageTemplate();
+
+			attachEditor();
 		};
 
-		var createTrigger = function() {
-			return $('<a />', {
-				class: 'aui-editor-trigger',
-				text: 'trigger'
-			})
+		var createTriggerTemplate = function() {
+			return $('<a class="aui-editor-trigger">' + Symphony.Language.get('Edit') + '</a>');
+		};
+
+		var createPageTemplate = function() {
+			return $('<div class="aui-editor-wrapper"><div class="aui-editor-page"><iframe class="hidden" width="100%" height="100%" frameborder="0" /></div></div>');
 		};
 
 		var attachEditor = function() {
-			var item = $(this).find('.item');
+			fields.each(attachTrigger);
+		};
 
-			if(!item.find('.aui-editor-trigger').length) {
-				item.prepend(trigger.clone());				
-			}
+		var attachTrigger = function() {
+			var item = $(this).find('.item'),
+				trigger = templateTrigger.clone();
+
+			trigger.on('click.aui-editor', loadPage);
+			item.not('.aui-editor-trigger').prepend(trigger);				
+		};
+
+		var loadPage = function() {
+			var page = templatePage.clone(),
+				iframe = page.find('iframe').attr('src', window.location).on('load.aui-editor', function() {
+					var iframe = $(this),
+						contents = iframe.contents();
+
+					contents.find('body').addClass('aui-editor-section');
+					contents.find('#header').remove();
+					contents.find('#context .actions').remove();
+
+					iframe.removeClass('hidden');
+				});
+
+			Symphony.Elements.contents.append(page);
 		};
 
 		// API
@@ -34,8 +59,8 @@
 		};
 	}();
 
-	$(document).on('ready.aui-editor', function() {
+	$(window).on('load.aui-editor', function() {
 		Symphony.Extensions.AssociationUIEditor.init();
-	});
+	})
 
 })(window.jQuery, window.Symphony);
