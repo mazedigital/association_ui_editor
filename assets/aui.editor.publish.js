3f0d846ca5f8b3592da95bf43366199686cc3193
Nils Hörrmann <post@nilshoerrmann.de>
2014-07-24 18:13:00 +0200

    Opening and closing actions + undo option

--- assets/aui.editor.publish.js
+++ assets/aui.editor.publish.js
@@ -3,28 +3,29 @@
 
 	Symphony.Language.add({
 		'Edit': false,
-		'Associated {$section-name}': false
+		'Associated {$section-name}': false,
+		'You just closed the editor for “{$title}”.': false
 	});
 
 	Symphony.Extensions.AssociationUIEditor = function() {
 		var fields,
-			templateTrigger, templatePage;
+			templateTrigger, templateEditor,
+			editors = {};
 
 		var init = function() {
 			fields = Symphony.Elements.contents.find('.field[data-editor^="aui-editor"]');
 			templateTrigger = createTriggerTemplate();
-			templatePage = createPageTemplate();
+			templateEditor = createEditorTemplate();
 
 			attachEditor();
 		};
 
 		var createTriggerTemplate = function() {
-			console.log('createTriggerTemplate');
 			return $('<a class="aui-editor-trigger">' + Symphony.Language.get('Edit') + '</a>');
 		};
 
-		var createPageTemplate = function() {
-			return $('<div class="aui-editor-wrapper"><div class="aui-editor-page"><iframe class="hidden" width="100%" height="100%" frameborder="0" /></div></div>');
+		var createEditorTemplate = function() {
+			return $('<div class="aui-editor"><div class="aui-editor-page"><iframe class="is-hidden" width="100%" height="100%" frameborder="0" /></div></div>');
 		};
 
 		var attachEditor = function() {
@@ -35,53 +36,146 @@
 			var item = $(this).find('.item'),
 				trigger = templateTrigger.clone();
 
-			trigger.on('click.aui-editor', loadPage);
+			trigger.on('mousedown.aui-editor', triggerPage);
 			item.not('.aui-editor-trigger').prepend(trigger);				
 		};
 
+		var triggerPage = function(event) {
+			var link = $(this).parent().data('link');
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			if(editors[link]) {
+				showEditor(link);
+			}
+			else {
+				loadEditor(link);
+			}
+		};
+
+		var loadEditor = function(link) {
+			var editor = templateEditor.clone();
+
+			// Prepare page
+			editor.find('iframe').attr('src', link).on('load.aui-editor', loadPage);
+
+			// Prepare editor closing
+			editor.on('click.aui-editor', closeEditor);
+
+			// Attach editor
+			editor.data('link', link);
+			editors[link] = editor;
+			showEditor(link);
+		};
+
+		var closeEditor = function() {
+			var editor = $(this),
+				link = editor.data('link');
+
+			editor.addClass('is-canceled');
+			resetScrollHeight();
+
+			// Hide wrapper
+			setTimeout(function() {
+				editor.addClass('is-hidden');
+			}, 500);
+
+			// Prepare undo
+			prepareUndo(link);
+		};
+
 		var loadPage = function() {
-			var trigger = $(this),
-				page = templatePage.clone(),
-				link = trigger.parents('.item').data('link');
+			var iframe = $(this),
+				editor = iframe.parents('.aui-editor'),
+				contents = iframe.contents(),
+				title = contents.find('#symphony-subheading').text(),
+				link = editor.data('link'),
+				height;
 
-			// Adjust nested section
-			page.find('iframe').attr('src', link).on('load.aui-editor', function() {
-				var iframe = $(this),
-					contents = iframe.contents();
+			// Store title
+			editor.data('title', title);
 
-				// Remove elements
-				contents.find('body').addClass('aui-editor-section');
-				contents.find('#header').remove();
-				contents.find('#context .actions').remove();
-				contents.find('#content .actions .delete').remove();
+			// Remove elements
+			contents.find('body').addClass('aui-editor-section');
+			contents.find('#header').remove();
+			contents.find('#context .actions').remove();
+			contents.find('#contents .actions .delete').remove();
 
-				// Modify content
-				contents.find('#breadcrumbs a').each(function(index) {
-					var link = $(this),
-						sectionName;
+			// Modify header links
+			contents.find('#breadcrumbs a').each(modifyHeader);
 
-					link.removeAttr('href');
+			// Show content
+			iframe.removeClass('is-hidden');
 
-					if(index === 0) {
-						name = link.text();
-						link.text(Symphony.Language.get('Associated {$section-name}', {
-							'section-name': name
-						}));
-					}
-				});
+			// Set height
+			height = contents.find('#wrapper').height();
+			editor.data('height', height);
+			setScrollHeight(link);
+		};
 
-				iframe.removeClass('hidden');
+		var modifyHeader = function(index) {
+			var link = $(this),
+				sectionName;
+
+			// Remove internal links
+			link.removeAttr('href');
+
+			// Set title
+			if(index === 0) {
+				link.text(Symphony.Language.get('Associated {$section-name}', {
+					'section-name': link.text()
+				}));
+			}
+		};
+
+		var showEditor = function(link) {
+			var editor = editors[link];
+
+			if(editor.is('.is-hidden')) {
+				editor.removeClass('is-hidden');
+				editor.removeClass('is-canceled');
+				setScrollHeight(link);
+			}
+			else {
+				Symphony.Elements.contents.append(editor);
+			}
+			
+			// Remove existing undo options
+			Symphony.Elements.header.find('.notifier .undo').trigger('detach.notify');
+		};
+
+		var prepareUndo = function(link) {
+			var id = new Date().getTime(),
+				editor = editors[link],
+				title = editor.data('title');
+
+			// Offer undo option after removing a field
+			Symphony.Elements.header.find('div.notifier').trigger(
+				'attach.notify', 
+				[Symphony.Language.get('You just closed the editor for “{$title}”.', {title: title}) + '<a id="' + id + '">' + Symphony.Language.get('Restore?') + '</a>', 'protected undo']
+			);
+
+			// Prepare field recovery
+			$('#' + id).on('click.aui-editor', function() {
+				showEditor(link);
+				$(this).parent().trigger('detach.notify');
 			});
+		};
 
-			// Cancel page
-			page.on('click.aui-editor', function() {
-				page.addClass('cancel');
-				setTimeout(function() {
-					page.remove();
-				}, 500);
+		var setScrollHeight = function(link) {
+			var editor = editors[link],
+				height = editor.data('height');
+
+			Symphony.Elements.contents.css({
+				minHeight: height,
+				maxHeight: height,
+				overflow: 'hidden'
 			});
+		};
 
-			Symphony.Elements.contents.append(page);
+		var resetScrollHeight = function() {
+			Symphony.Elements.contents.removeAttr('style');
 		};
 
 		// API
