1b658bb6e545707f7ff56bcb368e9a7af12d9294
Omid Sarei <omid@maze.digital>
2019-05-10 12:14:55 +0200

    php 7.x support

--- extension.driver.php
+++ extension.driver.php
@@ -2,7 +2,7 @@
 
 Class extension_association_ui_editor extends Extension
 {
-    private static $provides = array();
+    protected static $provides = array();
 
     public static function registerProviders()
     {
